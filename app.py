49fa4804b7ead4b0d650a18579523d65da57c929
RubiyaMhaisale <111623153+RubiyaMhaisale@users.noreply.github.com>
2022-09-27 14:41:35 -0400

    updated app.py
    added print statement at line 134

--- app.py
+++ app.py
@@ -131,6 +131,7 @@ if __name__ == '__main__':
     # Check if input color is a supported one
     if COLOR not in color_codes:
         print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
+        print("Assignment 1 Fall 2022 - Seneca College")
         exit(1)
 
     app.run(host='0.0.0.0',port=8080,debug=True)
